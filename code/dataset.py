b3f8a534c0e77746c54b2317753a89f086bc9b32
Shakib-IO <khansakib299@gmail.com>
2024-08-20 12:48:29 -0400

    Update code for all the folders in datasets

--- code/dataset.py
+++ code/dataset.py
@@ -27,65 +27,132 @@ class TILDataset:
         self.data = []
         self.image_extensions = {'.jpg', '.jpeg', '.png', '.bmp', '.gif'}
         
-        self.df = self.load_data()
-        
-    def load_data(self):
+
+    ### TRAINING DATA ####
+    def load_train_data(self):
         """
-        Load data from the root directory and its subdirectories.
+        Load training data from the root directory and its subdirectories.
 
         Returns:
         --------
         pd.DataFrame
             DataFrame containing file paths and labels.
         """
-        for sub_dir in self.sub_dirs:
-            sub_dir_path = os.path.join(self.root_dir, sub_dir)
-            if os.path.exists(sub_dir_path):
-                for class_dir, label in self.class_dirs.items():
-                    class_path = os.path.join(sub_dir_path, class_dir)
-                    if os.path.exists(class_path):
-                        for file_name in os.listdir(class_path):
-                            if file_name.lower().endswith(tuple(self.image_extensions)):
-                                file_path = os.path.join(class_path, file_name)
-                                self.data.append([file_path, label])
-            else:
-                for root_sub_dir in os.listdir(self.root_dir):
-                    sub_dir_path = os.path.join(self.root_dir, root_sub_dir, sub_dir)
-                    if os.path.exists(sub_dir_path):
-                        for class_dir, label in self.class_dirs.items():
-                            class_path = os.path.join(sub_dir_path, class_dir)
-                            if os.path.exists(class_path):
-                                for file_name in os.listdir(class_path):
-                                    if file_name.lower().endswith(tuple(self.image_extensions)):
+        for root_sub_dir in os.listdir(self.root_dir):
+            root_sub_dir_path = os.path.join(self.root_dir, root_sub_dir)
+            if os.path.isdir(root_sub_dir_path):
+                for root_sub_tvt_dir in os.listdir(root_sub_dir_path):
+                    # Process only 'test' directory
+                    if root_sub_tvt_dir.lower() == "train":
+                        root_sub_tvt_dir_path = os.path.join(root_sub_dir_path, root_sub_tvt_dir)                    
+                        if os.path.isdir(root_sub_tvt_dir_path):
+                            for class_dir, label in self.class_dirs.items():
+                                class_path = os.path.join(root_sub_tvt_dir_path, class_dir)
+                                if os.path.isdir(class_path):
+                                    for file_name in os.listdir(class_path):
                                         file_path = os.path.join(class_path, file_name)
-                                        self.data.append([file_path, label])
+                                        # Check if it's a file and has the correct extension
+                                        if os.path.isfile(file_path) and file_name.lower().endswith(tuple(self.image_extensions)):
+                                            self.data.append([file_path, label])
         
-        df = pd.DataFrame(self.data, columns=['file_path', 'label'])
-        if not df.empty:
+        df_train = pd.DataFrame(self.data, columns=['file_path', 'label'])
+        if not df_train.empty:
             print("Data loaded successfully")
-            return df
+            return df_train
         else:
             print("Data loading failed")
             return pd.DataFrame(columns=['file_path', 'label'])
 
-    
-    def convert_to_huggingface_dataset_format(self):
-        if not self.df.empty:
-            dataset = Dataset.from_pandas(self.df)
-            
-            train_test_valid = dataset.train_test_split(test_size=0.3, seed=42)
-            test_valid = train_test_valid['test'].train_test_split(test_size=0.6, seed=42)
+    ### VALDATION DATA ####
+    def load_val_data(self):
+        """
+        Load training data from the root directory and its subdirectories.
 
-            dataset_dict = DatasetDict({
-                'train': train_test_valid['train'],
-                'validation': test_valid['test'],
-                'test': test_valid['train']
-            })
-
-            return dataset_dict
+        Returns:
+        --------
+        pd.DataFrame
+            DataFrame containing file paths and labels.
+        """
+        for root_sub_dir in os.listdir(self.root_dir):
+            root_sub_dir_path = os.path.join(self.root_dir, root_sub_dir)
+            if os.path.isdir(root_sub_dir_path):
+                for root_sub_tvt_dir in os.listdir(root_sub_dir_path):
+                    # Process only 'test' directory
+                    if root_sub_tvt_dir.lower() == "val":
+                        root_sub_tvt_dir_path = os.path.join(root_sub_dir_path, root_sub_tvt_dir)                    
+                        if os.path.isdir(root_sub_tvt_dir_path):
+                            for class_dir, label in self.class_dirs.items():
+                                class_path = os.path.join(root_sub_tvt_dir_path, class_dir)
+                                if os.path.isdir(class_path):
+                                    for file_name in os.listdir(class_path):
+                                        file_path = os.path.join(class_path, file_name)
+                                        # Check if it's a file and has the correct extension
+                                        if os.path.isfile(file_path) and file_name.lower().endswith(tuple(self.image_extensions)):
+                                            self.data.append([file_path, label])
+        
+        df_val = pd.DataFrame(self.data, columns=['file_path', 'label'])
+        if not df_val.empty:
+            print("Data loaded successfully")
+            return df_val
         else:
-            print("Conversion failed: DataFrame is empty")
-            return None
+            print("Data loading failed")
+            return pd.DataFrame(columns=['file_path', 'label'])
+
+    #### TEST DATA ####
+    def load_test_data(self):
+        """
+        Load training data from the root directory and its subdirectories.
+
+        Returns:
+        --------
+        pd.DataFrame
+            DataFrame containing file paths and labels.
+        """
+        for root_sub_dir in os.listdir(self.root_dir):
+            root_sub_dir_path = os.path.join(self.root_dir, root_sub_dir)
+            if os.path.isdir(root_sub_dir_path):
+                for root_sub_tvt_dir in os.listdir(root_sub_dir_path):
+                    # Process only 'test' directory
+                    if root_sub_tvt_dir.lower() == "test":
+                        root_sub_tvt_dir_path = os.path.join(root_sub_dir_path, root_sub_tvt_dir)                    
+                        if os.path.isdir(root_sub_tvt_dir_path):
+                            for class_dir, label in self.class_dirs.items():
+                                class_path = os.path.join(root_sub_tvt_dir_path, class_dir)
+                                if os.path.isdir(class_path):
+                                    for file_name in os.listdir(class_path):
+                                        file_path = os.path.join(class_path, file_name)
+                                        # Check if it's a file and has the correct extension
+                                        if os.path.isfile(file_path) and file_name.lower().endswith(tuple(self.image_extensions)):
+                                            self.data.append([file_path, label])
+        
+        df_test = pd.DataFrame(self.data, columns=['file_path', 'label'])
+        if not df_test.empty:
+            print("Data loaded successfully")
+            return df_test
+        else:
+            print("Data loading failed")
+            return pd.DataFrame(columns=['file_path', 'label'])
+    
+
+    @staticmethod
+    def convert_to_huggingface_dataset_format():
+        
+        # Create an instance of the TILDataset
+        dataset_instance = TILDataset(root_dir='datasets/')
+
+        # # Load the training data
+        train_df = Dataset.from_pandas(dataset_instance.load_train_data())
+        # # Load the validation data
+        val_df = Dataset.from_pandas(dataset_instance.load_val_data())
+        # Load the test data
+        test_df = Dataset.from_pandas(dataset_instance.load_test_data())
+
+        dataset_dict = DatasetDict({
+            "train": train_df,
+            "validation": val_df,
+            "test": test_df
+        })
+        return dataset_dict
 
 
 # Load the Data
@@ -104,7 +171,10 @@ def load_dataset():
     dataset: Hugging Face dataset format
 
     """
-    til_data = TILDataset(root_dir='datasets/brca')
-    
-    huggingface_dataset = til_data.convert_to_huggingface_dataset_format()
+    huggingface_dataset = TILDataset(root_dir='datasets/').convert_to_huggingface_dataset_format()
     return huggingface_dataset
+
+d = load_dataset()
+print(d)
+print(d["train"]["file_path"][0])
+print(d["train"]["label"][0])
